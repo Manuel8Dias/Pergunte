d5154e883281c6c6f7879075b2ccedbab74a0833
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-08-21 20:17:28 +0100

    Commit bug fix

--- src/db/init.js
+++ src/db/init.js
@@ -7,19 +7,19 @@ const initDb = {
         await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
             pass TEXT
-        )`);
+        )`)
 
         await db.exec(`CREATE TABLE questions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
             read INT
-        )`);
+        )`)
 
         await db.close()
     }
 }
 
-initDb.init();
+initDb.init()
 
 
 
