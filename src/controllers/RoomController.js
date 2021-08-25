aaf2cc7662ac1d48e452a2b50b9d37b4ff4060f6
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-08-25 18:14:08 +0100

    Commit entrar em sala já existente

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -33,7 +33,7 @@ module.exports = {
 
         res.redirect(`/room/${roomId}`)
     },
-
+    
     async open(req, res) {
         const db = await Database()
         const roomId = req.params.room
@@ -42,5 +42,11 @@ module.exports = {
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`) 
 
         res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead })
-    }
-} 
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId
+
+        res.redirect(`/room/${roomId}`)
+    } 
+}
